b12aaa257e8499a65b05d5d2d6dec820cbdf022b
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2023-03-30 23:28:38 +0900

    First react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,80 @@
 import React from 'react';
-import { atom, selector, useRecoilValue } from "recoil";
-import axios from "axios";
+import styled from '@emotion/styled/macro';
+import Skeleton from "./components/Skeleton";
 
-const todoIdState = atom({
-    key: 'todoIdState',
-    default: 1
-});
+const Base = styled.div`
+    display: grid;
+    width: 100%;
+    grid-template-columns: repeat(5, 1fr);
+    column-gap: 12px;
+    row-gap: 24px;
+`;
 
-const todoItemQuery = selector({
-    key: 'todoItemQuery',
-    get: async ({get}) =>{
-        const id = get(todoIdState);
-        console.log(id);
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        console.log(response);
-        return response.data;
-    }
-})
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+    border-radius: 4px;
+`;
+
+const ImageWrapper = styled.div`
+    width: 100%;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+const Info = styled.div`
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0%;
+`;
+
+const Title = styled.h4`
+    margin: 0;
+    padding: 0;
+    font-size: 24px;
+`;
+
+const Description = styled.p`
+    margin: 8px 0 0 0;
+    padding: 0;
+    font-size: 16px;
+`;
+
+const Item: React.FC = () => {
+    return (
+        <Container>
+            <ImageWrapper>
+                <Image src="https://ichef.bbci.co.uk/news/640/cpsprodpb/E172/production/_126241775_getty_cats.png">
+                </Image>
+            </ImageWrapper>
+            <Info>
+                <Title>
+                    Cat Taking a nap
+                </Title>
+                <Description>
+                    zzzzzzzzzzzzzzzzzzzzzzzzzzz
+                </Description>
+            </Info>
+        </Container>
+    )
+
+}
 
 function App() {
-
-    const data = useRecoilValue(todoItemQuery);
-    return (
-        <div className="App">
-            {data.userId}
-            {data.title}
-        </div>
+  return (
+    <Base>
+        {
+            Array.from({length: 25}).map((_, idx) => (
+                <Item key={idx}></Item>
+            ))
+        }
+    </Base>
   );
 }
 
